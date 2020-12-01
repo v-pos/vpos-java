481835b8b53c8fbbd2d8d121b063485d0c5dd66a
Sergio Maziano <sergio@nextbss.co.ao>
2020-12-01 19:37:08 +0100

    Refactors the request

--- src/main/java/ao/vpos/vpos/Vpos.java
+++ src/main/java/ao/vpos/vpos/Vpos.java
@@ -2,8 +2,17 @@ package ao.vpos.vpos;
 
 import com.google.common.base.Preconditions;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+
+import java.net.URI;
 import java.net.URL;
+import java.util.HashMap;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import javax.annotation.Nonnull;
 
 public class Vpos {
@@ -18,125 +27,51 @@ public class Vpos {
     SANDBOX
   }
 
-  public static class Builder {
-    private URL baseUrl;
-    private Environment environment;
-    private String token;
-    private Integer posId;
-    private String supervisorCard; 
-
-    @Nonnull
-    public Builder posId(@Nonnull Integer posId) {
-      //Preconditions.checkNotNull(baseUrl, "posId can't be null");
-
-      this.posId = posId;
-      return this;
-    }
-
-    public Builder supervisorCard(@Nonnull String supervisorCard) {
-      //Preconditions.checkNotNull(supervisorCard, "supervisorCard can't be null");
-
-      this.supervisorCard = supervisorCard;
-      return this;
-    }
-
-    @Nonnull
-    public Builder baseUrl(@Nonnull String baseUrl) {
-      //Preconditions.checkNotNull(baseUrl, "baseUrl can't be null");
-
-      try {
-        this.baseUrl = new URL(baseUrl);
-
-        return this;
-      } catch (MalformedURLException e) {
-        throw new RuntimeException(e);
-      }
-    }
-
-    public Builder environment(Environment environment) {
-      if (environment == null) {
-        throw new IllegalArgumentException("environment can't be null");
-      }
-
-      this.environment = environment;
-      return this;
-    }
-
-    public Builder token(String token) {
-      //Preconditions.checkNotNull(token, "token can't be null");
-
-      this.token = token;
-      return this;
-    }
-
-    public Vpos build() {
-      if (environment == null && baseUrl == null) {
-        throw new IllegalArgumentException("vPOS baseUrl is required when environment is not specified");
-      }
-
-      if (baseUrl != null && baseUrl.toString().endsWith("/")) {
-        baseUrl = removeTrailingSlashFromUrl(baseUrl);
-      }
-
-      if (environment != null) {
-        baseUrl = getBaseUrlByEnvironment(environment);
-      }
-
-      return new Vpos(baseUrl, token);
-    }
-
-    @SuppressWarnings("unused")
-    private URL getBaseUrlByEnvironment(Environment environment) {
-      try {
-        if (environment == environment.PRODUCTION) {
-          return new URL(PRODUCTION_BASE_URL);
-        } else {
-          return new URL(SANDBOX_BASE_URL);
-        }
-      } catch (MalformedURLException e) {
-        throw new RuntimeException(e);
-      }
-    }
-
-    private URL removeTrailingSlashFromUrl(URL baseUrl) {
-      String baseUrlString = baseUrl.toString();
-      if (!baseUrlString.endsWith("/")) {
-        return baseUrl;
-      }
-
-      try {
-        int indexOfTrailingSlash = baseUrlString.lastIndexOf("/");
-        return new URL(baseUrlString.substring(0, indexOfTrailingSlash));
-      } catch (MalformedURLException e) {
-        throw new RuntimeException(e);
-      }
+  public Vpos(String token) throws IOException, InterruptedException  {
+    this.token = token;
+    try{
+      this.baseUrl = new URL(SANDBOX_BASE_URL);
+    } catch(MalformedURLException e) {
+      throw new RuntimeException(e);
     }
   }
 
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  Vpos(URL baseUrl, String token) {
-    if (baseUrl == null) {
-      throw new IllegalArgumentException("vPOS baseUrl is required");
+  public Vpos(Environment environment, String token) throws IOException, InterruptedException {
+    try{
+      this.baseUrl = (environment == Environment.SANDBOX) ? new URL(PRODUCTION_BASE_URL) : new URL(SANDBOX_BASE_URL);
+    } catch(MalformedURLException e) {
+      throw new RuntimeException(e);
     }
-
-    this.baseUrl = baseUrl;
     this.token = token;
   }
 
-  /**
-   * Retrieve vPOS base URL
-   */
-  public URL getBaseUrl() {
-    return baseUrl;
+  private String getToken() {
+    return token; 
   }
 
-  /**
-   * Retrieve vPOS admin authentication token.
-   */
-  public String getToken() {
-    return token;
+  private String getBaseUrl() {
+    return String.valueOf(baseUrl);
+  }
+
+  public HashMap<String, String> getAllTransactions() throws IOException, InterruptedException {
+    HttpClient client = HttpClient.newHttpClient();
+
+    HttpRequest request = HttpRequest.newBuilder()
+      .GET()
+      .header("Accept", "application/json")
+      .header("Content-Type", "application/json")
+      .header("Authorization", "Bearer " + token)
+      .uri(URI.create(getBaseUrl() + "/api/v1/transactions"))
+      .build();
+
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+    HashMap<String, String> returnedObject = new HashMap<String, String>();
+
+    returnedObject.put("data", String.valueOf(response.body()));
+    returnedObject.put("message", "not_defined"); 
+    returnedObject.put("code", String.valueOf(response.statusCode()));
+
+    return returnedObject;
   }
 }
