b56feac74d8dc5c7662a9f3eacbf503d18f5a02f
Sergio Maziano <sergio@nextbss.co.ao>
2020-12-02 01:08:57 +0100

    Update the project with a simplified approach

--- src/main/java/ao/vpos/vpos/Vpos.java
+++ src/main/java/ao/vpos/vpos/Vpos.java
@@ -4,6 +4,8 @@ import com.google.common.base.Preconditions;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Arrays;
+import java.util.List;
 
 import java.net.URI;
 import java.net.URL;
@@ -13,6 +15,7 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import ao.vpos.vpos.model.ReturnObject;
 import javax.annotation.Nonnull;
 
 public class Vpos {
@@ -68,10 +71,34 @@ public class Vpos {
 
     HashMap<String, String> returnedObject = new HashMap<String, String>();
 
-    returnedObject.put("data", String.valueOf(response.body()));
-    returnedObject.put("message", "not_defined"); 
     returnedObject.put("code", String.valueOf(response.statusCode()));
+    returnedObject.put("message", "OK"); 
+    returnedObject.put("data", String.valueOf(response.body()));
 
     return returnedObject;
+  }
+
+  public ReturnObject getTransaction(String transactionId) throws IOException, InterruptedException {
+    HttpClient client = HttpClient.newHttpClient();
+
+    HttpRequest request = HttpRequest.newBuilder()
+      .GET()
+      .header("Accept", "application/json")
+      .header("Content-Type", "application/json")
+      .header("Authorization", "Bearer " + token)
+      .uri(URI.create(getBaseUrl() + String.format("/api/v1/transactions/%s", transactionId)))
+      .build();
+    
+    //HashMap<String, String> returnedObject = new HashMap<String, String>();
+
+    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+    //returnedObject.put("code", String.valueOf(response.statusCode()));
+    //returnedObject.put("message", "OK"); 
+    //returnedObject.put("data", response.body());
+
+    ReturnObject returnedObject = new ReturnObject(response.statusCode(), "OK", response.body());
+
+    return returnedObject; 
   }
 }
