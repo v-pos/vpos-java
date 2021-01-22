3c9141331811213650417e047077eade66f27dfe
Alexandre Antonio Juca <corextechnologies@gmail.com>
2021-01-22 16:12:44 +0100

    implement TransactionViewModel

--- src/main/java/ao/vpos/vpos/model/TransactionViewModel.java
+++ src/main/java/ao/vpos/vpos/model/TransactionViewModel.java
@@ -1,2 +1,28 @@
-package ao.vpos.vpos.model;public class TransactionViewModel {
+package ao.vpos.vpos.model;
+
+public class TransactionViewModel implements ViewModel<Transaction>{
+    private final Integer statusCode;
+    private final String message;
+    private final Transaction data;
+
+    public TransactionViewModel(Integer statusCode, String message, Transaction data) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+    }
+
+    @Override
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public Transaction getData() {
+        return data;
+    }
 }
